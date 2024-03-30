509c21a768f6700f7d2b31a9cf4df3e5444e5ec5
NishideKenji <kngladsub@outlook.com>
2024-03-30 15:53:34 +0900

    Update DateInputGeneral.tsx

--- src/app/common/form/DateInputGeneral.tsx
+++ src/app/common/form/DateInputGeneral.tsx
@@ -1,21 +1,24 @@
-import { useField } from "formik";
-import React from "react";
-import { Form } from "react-bootstrap";
-import DatePicker, {ReactDatePickerProps} from 'react-datepicker';
+import { useField } from 'formik'
+import React from 'react'
+import { Form } from 'react-bootstrap'
+import type { ReactDatePickerProps } from 'react-datepicker'
+import DatePicker from 'react-datepicker'
 
-export default function DateInputGeneral(props: Partial<ReactDatePickerProps>){
-    const[field, meta, helpers] = useField(props.name!);
-    return (
-        <Form.Group>
-            <DatePicker
-                {...field}
-                {...props}
-                selected={(field.value && new Date(field.value)) || null}
-                onChange={value => helpers.setValue(value)}
-            />
-            {meta.touched && meta.error ? (
-                <Form.Label basic color='red'>{meta.error}</Form.Label>
-            ) : null}
-        </Form.Group>
-    )
+export default function DateInputGeneral(props: Partial<ReactDatePickerProps>) {
+  const [field, meta, helpers] = useField(props.name!)
+  return (
+    <Form.Group>
+      <DatePicker
+        {...field}
+        {...props}
+        selected={(field.value && new Date(field.value)) || null}
+        onChange={(value) => helpers.setValue(value)}
+      />
+      {meta.touched && meta.error ? (
+        <Form.Label basic color="red">
+          {meta.error}
+        </Form.Label>
+      ) : null}
+    </Form.Group>
+  )
 }
