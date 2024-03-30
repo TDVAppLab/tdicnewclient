61b97716bc2df99bd8909a6ffbf6f8969cc18d32
NishideKenji <kngladsub@outlook.com>
2024-03-30 16:05:06 +0900

    Update setStarlySky.ts

--- src/components/Articles/common/modelscreen/setStarlySky.ts
+++ src/components/Articles/common/modelscreen/setStarlySky.ts
@@ -1,24 +1,28 @@
-import { BufferGeometry, Float32BufferAttribute, MathUtils, Points, PointsMaterial, Scene } from "three";
-
-
-
-
+import type { Scene } from 'three'
+import {
+  BufferGeometry,
+  Float32BufferAttribute,
+  MathUtils,
+  Points,
+  PointsMaterial,
+} from 'three'
 
 //Lightを設定する。
-export function setStarrySky(scene: Scene){
-    const geometry = new BufferGeometry();
-    const vertices = [];
+export function setStarrySky(scene: Scene) {
+  const geometry = new BufferGeometry()
+  const vertices = []
 
-    for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < 10000; i++) {
+    vertices.push(MathUtils.randFloatSpread(2000)) // x
+    vertices.push(MathUtils.randFloatSpread(2000)) // y
+    vertices.push(MathUtils.randFloatSpread(2000)) // z
+  }
 
-        vertices.push(MathUtils.randFloatSpread(2000)); // x
-        vertices.push(MathUtils.randFloatSpread(2000)); // y
-        vertices.push(MathUtils.randFloatSpread(2000)); // z
+  geometry.setAttribute('position', new Float32BufferAttribute(vertices, 3))
 
-    }
-
-    geometry.setAttribute('position', new Float32BufferAttribute(vertices, 3));
-
-    const particles = new Points(geometry, new PointsMaterial({ color: 0x888888 }));
-    scene.add(particles);
+  const particles = new Points(
+    geometry,
+    new PointsMaterial({ color: 0x888888 }),
+  )
+  scene.add(particles)
 }
